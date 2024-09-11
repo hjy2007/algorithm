a9060ea0da71d379c6f428593ccd2732ed7709c5
Jeong-yeong <wjddud2007@gmail.com>
2024-09-11 10:19:08 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,8 @@
+
+function solution(s) {
+    if(s.length != 4 && s.length != 6) return false;
+    for(var i = 0; i < s.length; i++) {
+        if(isNaN(Number(s[i]))) return false;
+    }
+    return true;
+}
