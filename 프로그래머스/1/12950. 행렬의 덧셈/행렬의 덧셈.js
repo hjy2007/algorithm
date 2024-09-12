2b5c95c77e823ad523618e72d31a1a6dd2f8caa5
Jeong-yeong <wjddud2007@gmail.com>
2024-09-12 09:09:05 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.24 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,13 @@
+function solution(arr1, arr2) {
+    let answer = [[]]
+      
+    
+    for (i = 0;i < arr1.length;i++) {        
+        answer[i] = []
+        for (j = 0;j < arr1[0].length;j++) {
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+        }
+    }
+    
+    return answer;
+}
